307719db68a1ec8d9be6256eb45168676569aefb
benjamineruvieru <benjamineruvieru@gmail.com>
2023-01-25 17:09:59 +0100

    lint fix

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,6 @@
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Perform API call here and resolve or reject the promise
-    // based on the response
-    resolve({
-      status: 200,
-      body: "photo-profile-1",
-    });
+  return Promise.resolve({
+    status: 200,
+    body: "photo-profile-1",
   });
 }
