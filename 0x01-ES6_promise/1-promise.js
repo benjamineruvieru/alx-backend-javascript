394c28e6eeec941e0b651d5e05c6e604cb9292b4
benjamineruvieru <benjamineruvieru@gmail.com>
2023-01-26 05:16:17 +0100

    task2

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+function getFullResponseFromAPI(success) {
+    if (success){
+    return Promise.resolve({
+      status: 200,
+      body: "Success",
+    });
+} else {
+    return Promise.reject(
+     'The fake API is not working currently'
+      );
+}
+  }
+  
