f2d671b1be8b500c72df97226cb02769c16114b8
Benjamin Eruvieru <benjamineruvieru@gmail.com>
2023-01-26 23:50:29 +0100

    task 1 -4

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,14 @@
+/**
+ * Javascript Class
+ */
+export default class ClassRoom {
+    /**
+     * Creates a new @see {@link ClassRoom}.
+     * @param {Number} maxStudentsSize - The maximum number of
+     * students in the class.
+     */
+    constructor(maxStudentsSize) {
+      this._maxStudentsSize = maxStudentsSize;
+    }
+  }
+  
